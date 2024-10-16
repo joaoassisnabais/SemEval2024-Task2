578d4be9079d81b41823af24b860d1262ff52d68
joaoassisnabais <joao.m.nabais@tecnico.ulisboa.pt>
2024-10-16 13:57:46 +0100

    Create new sc prompt without output format

--- run_inference.py
+++ run_inference.py
@@ -116,7 +116,7 @@ def main():
     qrels = json.load(open(args.qrels))
 
     if args.task == "self_consistency":
-        prompt = init_prompt(args.prompts, "self_consistency_prompt", "binary_output_format")
+        prompt = init_prompt(args.prompts, "self_consistency_wo_output", "binary_output_format")
         
         majority_eval_prompt = json.load(open(args.prompts))["new_majority_evaluator_prompt"]
         self_consistency.self_consistency(model, tokenizer, queries, qrels, "self_consistency_prompt", prompt,
